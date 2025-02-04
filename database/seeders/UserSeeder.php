d348b9ab813477581fbba7624f9662d4854e6997
Mursaleen Umar <45422628+mursaleen123@users.noreply.github.com>
2025-02-04 10:07:10 +0500

    Added UserSeeder to create an admin and 100 fake users

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Faker\Factory as Faker;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password'),
+            'role' => 'admin',
+        ]);
+
+        // Create 100 fake users
+        $faker = Faker::create();
+
+        for ($i = 0; $i < 100; $i++) {
+            User::create([
+                'name' => $faker->name(),
+                'email' => $faker->unique()->safeEmail(),
+                'password' => Hash::make('password'),
+                'role' => 'user',
+            ]);
+        }
+    }
+}
